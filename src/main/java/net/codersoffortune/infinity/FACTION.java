3a51c86172a5221b48d7f7fc7d04c7db80b3c01a
Oliver Matthews <oliver@codersoffortune.net>
2020-11-12 10:11:21 +0000

    Add all the factions and sectorals

--- src/main/java/net/codersoffortune/infinity/FACTION.java
+++ src/main/java/net/codersoffortune/infinity/FACTION.java
@@ -13,8 +13,16 @@ import java.util.stream.Collectors;
  */
 public enum FACTION {
     PanOceania(1, "PanOceania", Arrays.asList(SECTORAL.PanO, SECTORAL.MO, SECTORAL.Varuna, SECTORAL.Svaralheima),"00B0F2", "\"r\": 0.0, \"g\": 0.6901961, \"b\": 0.9490196", "http://cloud-3.steamusercontent.com/ugc/1685996137035355524/04967D96ACBEBB645E5FA5F365BB6D7AEBCC06CC/"),
-    YuJing(2, "Yu Jing", Arrays.asList(SECTORAL.YuJing, SECTORAL.IS, SECTORAL.IA, SECTORAL.WhiteBanner), "FF9000", "\"r\": 1.0, \"g\": 0.5647059, \"b\": 0.0", "http://cloud-3.steamusercontent.com/ugc/1685996137035358127/F98F9BDC03079C010366C6239DF43D8CABA5BFCE/");
-    //Ariadna(3, "Ariadna", Arrays.asList(301,303,304), "007B25", "\"r\": 0.0, \"g\": 0.4823529, \"b\": 0.145097345", "http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/");
+    YuJing(2, "Yu Jing", Arrays.asList(SECTORAL.YuJing, SECTORAL.IS, SECTORAL.IA, SECTORAL.WhiteBanner), "FF9000", "\"r\": 1.0, \"g\": 0.5647059, \"b\": 0.0", "http://cloud-3.steamusercontent.com/ugc/1685996137035358127/F98F9BDC03079C010366C6239DF43D8CABA5BFCE/"),
+    Ariadna(3, "Ariadna", Arrays.asList(SECTORAL.Ariadna, SECTORAL.USAriadna, SECTORAL.TAC, SECTORAL.Kosmoflot), "007B25", "\"r\": 0.0, \"g\": 0.4823529, \"b\": 0.145097345", "http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/"),
+    Haqqislam(4,"Haqqislam", Arrays.asList(SECTORAL.Haqq, SECTORAL.Hassassins, SECTORAL.Ramah),"E6DA9B", "\"r\": 0.9019608, \"g\": 0.854901969, \"b\": 0.607843161", "http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/"),
+    Nomads(5,"Nomads", Arrays.asList(SECTORAL.Nomads, SECTORAL.Corregidor, SECTORAL.Bakunin, SECTORAL.Tunguska), "ce181f", "\"r\": 0.807843149, \"g\": 0.09411695, \"b\": 0.1215679", "http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/"),
+    CombinedArmy(6,"Combined Army",Arrays.asList(SECTORAL.CA, SECTORAL.MAF, SECTORAL.Onyx, SECTORAL.Shasvastii), "813CAA", "\"r\": 0.5058824, \"g\": 0.235293388, \"b\": 0.6666667", "http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/"),
+    Aleph(7, "Aleph",Arrays.asList(SECTORAL.Aleph, SECTORAL.OSS),"AFA7BC", "\"r\": 0.6862744, \"g\": 0.654902, \"b\": 0.737255","http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/"),
+    // TODO:: Handle NA2 colours properly.
+    NA2(9, "Non-Aligned Armies",Arrays.asList(SECTORAL.Druze, SECTORAL.JSA, SECTORAL.Ikari, SECTORAL.Starco, SECTORAL.SpiralCorps, SECTORAL.FoCo, SECTORAL.Dahshat, SECTORAL.WiCo),"f25255", "\"r\": 0.32700938, \"g\": 0.46554023, \"b\": 0.4444081", "http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/"),
+    O12(10, "O-12",Arrays.asList(SECTORAL.O12, SECTORAL.Starmarda), "726C96", "\"r\": 0.305881649, \"g\": 0.27843067, \"b\": 0.4666664", "http://cloud-3.steamusercontent.com/ugc/929303676573612409/FF751A206976EBD2B75A29196FDCBB590ECB0A43/");
+
 
     // TODO:: Finish adding the factions
 
--- src/main/java/net/codersoffortune/infinity/SECTORAL.java
+++ src/main/java/net/codersoffortune/infinity/SECTORAL.java
@@ -9,7 +9,35 @@ public enum SECTORAL {
     YuJing(201, "Yu Jing", 2),
     IS(202, "Imperial Service", 2),
     IA(204, "Invincible Army", 2),
-    WhiteBanner(205, "White Banner", 2);
+    WhiteBanner(205, "White Banner", 2),
+    Ariadna(301, "Ariadna", 3),
+    USAriadna(304, "USAriadna Ranger Force", 3),
+    TAC(305, "Tartary Army Corps", 3),
+    Kosmoflot(306, "Kosmoflot", 3),
+    Haqq(401, "Haqqislam", 4),
+    Hassassins(402, "Hassassin Bahram", 4),
+    Ramah(404, "Ramah Taskforce", 4),
+    Nomads(501, "Nomads", 5),
+    Corregidor(502, "Jurisdictional Command of Corregidor", 5),
+    Bakunin(503, "Jurisdictional Command of Bakunin", 5),
+    Tunguska(504, "Jurisdictional Command of Tunguska", 5),
+    CA(601, "Combined Army", 6),
+    MAF(602, "Morat Aggression Force", 6),
+    Shasvastii(603, "Shasvastii Expeditionary Force", 6),
+    Onyx(604, "Onyx Contact Force", 6),
+    Aleph(701, "Aleph", 7),
+    OSS(703, "Operations Subsection of the SSS", 7),
+    //NA2(901, "Non-Aligned Armies", 9)
+    Druze(902, "Druze Bayram Security", 9),
+    JSA(903, "Japanese Secessionist Army", 9),
+    Ikari(904, "Ikari Company", 9),
+    Starco(905, "Starco. Free Company of the Star", 9),
+    SpiralCorps(906, "Spiral Corps", 9),
+    FoCo(907, "Foreign Company", 9),
+    Dahshat(908, "Dahshat Company", 9),
+    WiCo(909, "White Company", 9),
+    O12(1001, "O-12", 10),
+    Starmarda(1002, "Starmada, Bureau Aegis Naval Police Department", 10);
 
     private final int id;
     private final String name;
