97e4c7603c2ef398b55752c5b93bb126ef2f770c
Oliver Matthews <oliver@codersoffortune.net>
2020-11-13 10:05:15 +0000

    Improve TTSModel comparison

--- src/main/java/net/codersoffortune/infinity/tts/TTSModel.java
+++ src/main/java/net/codersoffortune/infinity/tts/TTSModel.java
@@ -1,11 +1,18 @@
 package net.codersoffortune.infinity.tts;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TTSModel {
     private String decals;
     private String meshes;
     private String name; // mostly a sanity check
+
+    private static Pattern decalPattern = Pattern.compile("\"ImageURL\":\"(?<url>[^\"]*)\"");
+    private static Pattern meshPattern = Pattern.compile("\"MeshURL\":\"(?<url>[^\"]*)\"");
 
     public TTSModel() {
     }
@@ -36,13 +43,31 @@ public class TTSModel {
         this.decals = decals;
     }
 
+    private static Set<String> extractDecals(String decals) {
+        Set<String> result = new HashSet<>();
+        Matcher m = decalPattern.matcher(decals);
+        while( m.find() ) {
+            result.add(m.group("url"));
+        }
+        return result;
+    }
+
+    private static Set<String> extractMeshes(String meshes) {
+        Set<String> result = new HashSet<>();
+        Matcher m = meshPattern.matcher(meshes);
+        while( m.find() ) {
+            result.add(m.group("url"));
+        }
+        return result;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TTSModel ttsModel = (TTSModel) o;
-        return Objects.equals(decals, ttsModel.decals) &&
-                Objects.equals(meshes, ttsModel.meshes);
+        return extractDecals(decals).equals(extractDecals(ttsModel.decals)) &&
+                extractMeshes(meshes).equals(extractMeshes(ttsModel.meshes));
     }
 
     @Override
