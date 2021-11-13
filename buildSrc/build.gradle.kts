cb30e0cefb333586b6f1d35879e2ab40ee8f0084
Oliver Matthews <ojm@codersoffortune.net>
2021-11-13 19:34:23 +0000

    Add kotlin libraries

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,9 +5,15 @@
 plugins {
     // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
     `kotlin-dsl`
+    kotlin("jvm") version "1.6.0-RC2"
+
 }
 
 repositories {
     // Use the plugin portal to apply community plugins in convention plugins.
     gradlePluginPortal()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
 }
