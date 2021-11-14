e80096be9701d1577a0d900efc3e36c5a0ab8034
Oliver Matthews <ojm@codersoffortune.net>
2021-11-14 13:13:19 +0000

    Started building database edit page

--- Gui/src/main/kotlin/net/codersoffortune/infinity/gui/CatalogueController.kt
+++ Gui/src/main/kotlin/net/codersoffortune/infinity/gui/CatalogueController.kt
@@ -1,4 +1,25 @@
 package net.codersoffortune.infinity.gui
 
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.ChoiceBox
+import javafx.stage.Stage
+import net.codersoffortune.infinity.FACTION
+import net.codersoffortune.infinity.db.Database
+
+
 class CatalogueController {
+    private val database : Database = Database.getInstance()
+
+    @FXML
+    private lateinit var factionChoiceBox: ChoiceBox<String>
+
+    @FXML
+    fun initialize() {
+        enumValues<FACTION>().forEach { factionChoiceBox.items.add(it.name) }
+    }
+
+
 }
