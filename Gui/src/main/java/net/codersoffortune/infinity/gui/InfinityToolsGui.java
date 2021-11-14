a112b6b38adb3c8e840586c731c639e6022bd144
Oliver Matthews <ojm@codersoffortune.net>
2021-11-14 10:47:15 +0000

    Add support for multiple windows

--- Gui/src/main/java/net/codersoffortune/infinity/gui/InfinityToolsGui.java
+++ Gui/src/main/java/net/codersoffortune/infinity/gui/InfinityToolsGui.java
@@ -3,23 +3,29 @@ package net.codersoffortune.infinity.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class InfinityToolsGui extends Application {
+    private static Stage stage = null;
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(InfinityToolsGui.class.getResource("/Main.fxml"));
-        VBox anchorPane = loader.load();
+        Pane anchorPane = FXMLLoader.load(InfinityToolsGui.class.getResource("/Main.fxml"));
         Scene scene = new Scene(anchorPane);
         stage.setTitle("Infinity Tools Gui");
         stage.setScene(scene);
         stage.show();
+        InfinityToolsGui.stage = stage;
+    }
 
+    public static void switchWindow(String name) throws IOException {
+        String fileName = String.format("/%s.fxml", name);
+        Pane pane = FXMLLoader.load(InfinityToolsGui.class.getResource(fileName));
+        stage.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("jvm") version "1.5.31" apply false
+    kotlin("plugin.serialization") version "1.5.31" apply false
+    id("org.openjfx.javafxplugin") version "0.0.10" apply false
+}
