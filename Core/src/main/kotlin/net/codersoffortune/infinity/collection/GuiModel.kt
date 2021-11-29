feaab52aaf2ea1c4882297382c3de60ba669a940
Oliver Matthews <ojm@codersoffortune.net>
2021-11-29 16:33:40 +0000

    Add printableunits to guimodel for transmuted models

--- Core/src/main/kotlin/net/codersoffortune/infinity/collection/GuiModel.kt
+++ Core/src/main/kotlin/net/codersoffortune/infinity/collection/GuiModel.kt
@@ -3,6 +3,9 @@ package net.codersoffortune.infinity.collection
 import net.codersoffortune.infinity.SECTORAL
 import net.codersoffortune.infinity.metadata.unit.CompactedUnit
 import net.codersoffortune.infinity.metadata.unit.PrintableUnit
+import net.codersoffortune.infinity.metadata.unit.TransmutedCompactedUnit
+import java.awt.print.Printable
+import kotlin.streams.toList
 
 /**
  * A GuiModel is basically a compacted unit, but presents itself for GUI consumption
@@ -10,6 +13,14 @@ import net.codersoffortune.infinity.metadata.unit.PrintableUnit
 data class GuiModel(val unit: CompactedUnit, val sectoral: SECTORAL) {
     val printableUnit: PrintableUnit by lazy {
         unit.getPrintableUnit(sectoral)
+    }
+
+    val printableUnits: Collection<PrintableUnit> by lazy {
+        if (unit is TransmutedCompactedUnit ) {
+            unit.compactedUnits.stream().map {it.getPrintableUnit(sectoral)}.toList()
+        } else {
+            listOf(printableUnit)
+        }
     }
 
     override fun toString(): String {
