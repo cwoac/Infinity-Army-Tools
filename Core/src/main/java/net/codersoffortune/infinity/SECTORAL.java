3e19d2ce75787cff06d042bf02c3c2c4bb02ed6c
Oliver Matthews <ojm@codersoffortune.net>
2021-11-29 14:47:37 +0000

    Fix C1 faction names

--- Core/src/main/java/net/codersoffortune/infinity/SECTORAL.java
+++ Core/src/main/java/net/codersoffortune/infinity/SECTORAL.java
@@ -50,8 +50,8 @@ public enum SECTORAL {
     //CODEONE(10000, "Code One", 100)
     C1PanO(10101, "PanOceania (C1)", 10000, "00B0F2", "\"r\": 0.0, \"g\": 0.6901961, \"b\": 0.9490196", "\"r\": 0.0, \"g\": 0.35, \"b\": 0.47", "http://cloud-3.steamusercontent.com/ugc/1685996137035355524/04967D96ACBEBB645E5FA5F365BB6D7AEBCC06CC/"),
     C1YuJing(10201, "Yu Jing (C1)", 10000, "FF9000", "\"r\": 1.0, \"g\": 0.5647059, \"b\": 0.0", "\"r\": 0.5, \"g\": 0.23, \"b\": 0.0", "http://cloud-3.steamusercontent.com/ugc/1685996137035358127/F98F9BDC03079C010366C6239DF43D8CABA5BFCE/"),
-    C1Ariadna(10301, "Ariadna", 10000, "007B25", "\"r\": 0.0, \"g\": 0.4823529, \"b\": 0.145097345", "\"r\": 0.0, \"g\": 0.24, \"b\": 0.07", "http://cloud-3.steamusercontent.com/ugc/1685996137035360244/4D42A1D4EA0DF0FA8298EBBF3A81426B962D2175/"),
-    C1Nomads(10501, "Nomads", 10000, "ce181f", "\"r\": 0.807843149, \"g\": 0.09411695, \"b\": 0.1215679", "\"r\": 0.4, \"g\": 0.05, \"b\": 0.06", "http://cloud-3.steamusercontent.com/ugc/1685996137035363314/141B310D2B3842D0CA9E79901B72B0EAABB0D5C3/"),
+    C1Ariadna(10301, "Ariadna (C1)", 10000, "007B25", "\"r\": 0.0, \"g\": 0.4823529, \"b\": 0.145097345", "\"r\": 0.0, \"g\": 0.24, \"b\": 0.07", "http://cloud-3.steamusercontent.com/ugc/1685996137035360244/4D42A1D4EA0DF0FA8298EBBF3A81426B962D2175/"),
+    C1Nomads(10501, "Nomads (C1)", 10000, "ce181f", "\"r\": 0.807843149, \"g\": 0.09411695, \"b\": 0.1215679", "\"r\": 0.4, \"g\": 0.05, \"b\": 0.06", "http://cloud-3.steamusercontent.com/ugc/1685996137035363314/141B310D2B3842D0CA9E79901B72B0EAABB0D5C3/"),
     C1CA(10601, "Combined Army (C1)", 10000, "813CAA", "\"r\": 0.5058824, \"g\": 0.235293388, \"b\": 0.6666667", "\"r\": 0.25, \"g\": 0.12, \"b\": 0.33", "http://cloud-3.steamusercontent.com/ugc/1685996137035365049/68902014928ED2D5F9C276A1254260D395EE9020/"),
     C1O12(11001, "O-12 (C1)", 10000, "726C96", "\"r\": 0.305881649, \"g\": 0.27843067, \"b\": 0.4666664", "\"r\": 0.15, \"g\": 0.14, \"b\": 0.23", "http://cloud-3.steamusercontent.com/ugc/1685996137035366882/D545AFAB6DEA17368E442BD2B7E256C4E11E4751/");
     private final int id;
